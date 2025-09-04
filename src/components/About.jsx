da2ee14d31d9f1555a26c527ca6e402e3a590425
Barbaracoosta <babiicoosta2@gmail.com>
2025-09-04 13:27:20 -0300

    corrigindo about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
       <div className="about-content">
         <div className="about-text">
           <p>
-            Sou <strong>Adrielle Alves</strong>, Sou Adrielle Alves, psicóloga clínica (CRP 02/30430), dedicada ao cuidado da saúde mental com foco em crianças e adolescentes. Minha missão é oferecer um espaço seguro e acolhedor, onde cada pessoa possa se expressar livremente e encontrar caminhos para lidar com suas emoções e desafios. Acredito que a psicologia tem o poder de transformar vidas e que, através da escuta e do acolhimento,
+            Sou <strong>Adrielle Alves</strong>, psicóloga clínica (CRP 02/30430), dedicada ao cuidado da saúde mental com foco em crianças e adolescentes. Minha missão é oferecer um espaço seguro e acolhedor, onde cada pessoa possa se expressar livremente e encontrar caminhos para lidar com suas emoções e desafios. Acredito que a psicologia tem o poder de transformar vidas e que, através da escuta e do acolhimento,
              é possível promover desenvolvimento, bem-estar e qualidade de vida. 
             Sou especializada em diferentes áreas que fortalecem minha prática clínica,
              como TCC (Terapia Cognitivo-Comportamental), Neuropsicologia, Avaliação Psicológica, Psicodiagnóstico e 
