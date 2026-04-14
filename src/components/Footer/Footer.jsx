ad0339431e65869b64511576ca89de7b8e42fcca
Barbaracoosta <babiicoosta2@gmail.com>
2026-04-14 16:39:19 -0300

    Ajuste de tamanho, titulo, texto e espaçamento em Benefits e footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,27 +2,32 @@ import { FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
-  const currentYear = new Date().getFullYear(); 
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="footer">
       <div className="footer-content">
-                 
-         <div className="footer-links"> 
-            <a href="#hero">Início</a>      
-            <a href="#about">Sobre</a>
-            <a href="#benefits">Terapia</a>              
-            <a href="#faq">Faq</a>
-          </div>
-          <p>© {currentYear} - Adrielle Alves Psicóloga Clínica - CRP 02/30430 | Todos os direitos reservados</p>
-          <div className="footer-socials">          
-            <a href="https://instagram.com/adriellealves.psi" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-              <FaInstagram />
-            </a>        
-
-          </div>
-          
-       </div> 
+        <div className="footer-links">
+          <a href="#hero">Início</a>
+          <a href="#about">Sobre</a>
+          <a href="#benefits">Terapia</a>
+          <a href="#faq">Faq</a>
+        </div>
+        <p>
+          © {currentYear} - Adrielle Alves Psicóloga Clínica - CRP 02/30430 |
+          Todos os direitos reservados
+        </p>
+        <div className="footer-socials">
+          <a
+            href="https://instagram.com/adriellealves.psi"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
