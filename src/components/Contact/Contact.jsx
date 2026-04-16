f16e72376fca4cada40274206857679eac7719c5
Barbaracoosta <babiicoosta2@gmail.com>
2026-04-16 16:34:27 -0300

    Adicionando tag ao titulo e alinhando icons em contato

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,17 +10,22 @@ import {
 function Contact() {
   return (
     <section id="contact" className="contact">
+      <div className="headerBenefits">
+        <span className="badge">Comunicação</span>
+        <h1 className="title">Entre em Contato</h1>
+        <p className="subtitle">
+          Atendimentos presenciais em Olinda/PE e on-line para todo Brasil.        
+        </p>
+      </div>
+
       <div className="contact-container">
         <div className="foto-container">
           <img src="images/Foto2.jpeg" alt="Adrielle" className="foto" />
           <p className="legenda">Adrielle Alves - CRP 02/30430</p>
-        </div>
+        </div>          
 
-        <div className="cards-container">
-          <h2 className="titulo">Contato</h2>
-          <p>
-            Atendimentos presenciais em Olinda/PE e on-line para todo Brasil.
-          </p>
+        <div className="cards-container">          
+            
           <div className="card-contact">
             <FaWhatsapp className="icon whatsapp" />
             <p>
